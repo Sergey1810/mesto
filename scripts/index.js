73f3c03f548df77698a86f66fa9d4770cbf18a1c
Sergey Lisovcev <deadlook15@yandex.ru>
2023-01-21 16:39:15 +0300

    fix: ошибки 1 ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,34 +1,37 @@
 
 let formElement = document.querySelector('.popup');
-let nameInput = formElement.querySelector('.popup__input-name');
-let jobInput = formElement.querySelector('.popup__input-job');
+let nameInput = formElement.querySelector('.popup__input_type_name');
+let jobInput = formElement.querySelector('.popup__input_type_job');
 let createProfileBtn = document.querySelector('.profile__create-btn');
 let closePopupBtn = document.querySelector('.popup__btn-close');
 let profileName = document.querySelector('.profile__title');
 let profileJob = document.querySelector('.profile__subtitle');
+let form = document.querySelector('.popup__form')
 
 createProfileBtn.addEventListener('click', openPopup);
 closePopupBtn.addEventListener('click',closePopup);
 
 function openPopup(){
     formElement.classList.add('popup_open');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
 }
 
 function closePopup(){
     formElement.classList.remove('popup_open');
 }
 
-nameInput.value = profileName.innerHTML;
-jobInput.value = profileJob.innerHTML;
+nameInput.value = profileName.textContent;
+jobInput.value = profileJob.textContent;
 
 function handleFormSubmit (evt) {
     evt.preventDefault(); 
 
-    profileName.textContent = `${nameInput.value}`;
-    profileJob.textContent = `${jobInput.value}`;
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
 
     closePopup()
 }
 
 
-formElement.addEventListener('submit', handleFormSubmit); 
+form.addEventListener('submit', handleFormSubmit); 
