b1fd720c9d82899ca45dda8228f2122229f044ff
Sergey Lisovcev <deadlook15@yandex.ru>
2023-02-03 15:51:53 +0300

    feat: Добавить card

--- scripts/index.js
+++ scripts/index.js
@@ -25,34 +25,81 @@ const initialCards = [
     }
   ]; 
 
-let formElement = document.querySelector('.popup');
-let formCard = document.querySelector('.popup-card');
-let nameInput = formElement.querySelector('.popup__input_type_name');
-let jobInput = formElement.querySelector('.popup__input_type_job');
-let titleInput = formCard.querySelector('.popup__input_type_name-card');
-let urlInput = formCard.querySelector('.popup__input_type_url');
-let createProfileBtn = document.querySelector('.profile__create-btn');
-let createCardBtn = document.querySelector('.profile__add-btn');
-let closeCardBtn = document.querySelector('.popup__btn-card-close')
-let addCardBtn = document.querySelector('.popup__btn-card-create')
-let closePopupBtn = document.querySelector('.popup__btn-close');
-let profileName = document.querySelector('.profile__title');
-let profileJob = document.querySelector('.profile__subtitle');
-let form = document.querySelector('.popup__form');
-let formCards = document.querySelector('.popup__form_type_card');
-let elements = document.querySelector('.elements');
-let popupImage = document.querySelector('.popup__image');
-let formImage = document.querySelector('.popup-image');
-let formImageSubtitle = document.querySelector('.popup__subtitle')
-let template = document.querySelector('#card').content.querySelector('.element');
-let imageClose = document.querySelector('.popup__btn-image-close')
+  let createProfileBtn = document.querySelector('.profile__create-btn');
+  let createCardBtn = document.querySelector('.profile__add-btn');
+  let closeCardBtn = document.querySelector('.popup__btn-card-close')
+  let addCardBtn = document.querySelector('.popup__btn-card-create')
+  let closePopupBtn = document.querySelector('.popup__btn-close');
+  let profileName = document.querySelector('.profile__title');
+  let profileJob = document.querySelector('.profile__subtitle');
+  let formElement = document.querySelector('.popup');
+  let popupCard = document.querySelector('.popup-card');
+  let nameInput = formElement.querySelector('.popup__input_type_name');
+  let jobInput = formElement.querySelector('.popup__input_type_job');
+  let titleInput = popupCard.querySelector('.popup__input_type_name-card');
+  let urlInput = popupCard.querySelector('.popup__input_type_url');
+  let form = document.querySelector('.popup__form');
+  let formCards = document.querySelector('.popup__form_type_card');
+  let elements = document.querySelector('.elements');
+  let popupImage = document.querySelector('.popup__image');
+  let formImage = document.querySelector('.popup-image');
+  let formImageSubtitle = document.querySelector('.popup__subtitle')
+  let template = document.querySelector('#card').content.querySelector('.element');
+  let imageClose = document.querySelector('.popup__btn-image-close')
+
+function handleCardSubmit (evt) {
+    evt.preventDefault();
+    let name = titleInput.value;
+    let link = urlInput.value;
+    const card = template.cloneNode(true)
+    card.querySelector('.element__image').src = link
+    card.querySelector('.element__image').addEventListener('click', function() {
+      popupImage.src = link
+      formImageSubtitle.textContent = name
+      formImage.classList.add('popup_open')
+    })
+    card.querySelector('.element__title').textContent = name;
+    let like = card.querySelector('.element__like')
+    like.addEventListener('click',function() {like.classList.toggle('element__like_active')});
+    let clearCard = card.querySelector('.element__clear') 
+    clearCard.addEventListener('click',function() {
+        card.remove();
+        
+    })
+    elements.prepend(card)
+    closePopupCard()
+}
+
+function renderCards(arr) {
+   const cards = arr.map(function (item) {
+    const card = template.cloneNode(true)
+    card.querySelector('.element__image').src = item.link
+    card.querySelector('.element__image').addEventListener('click', function() {
+      popupImage.src = item.link
+      formImageSubtitle.textContent = item.name
+      formImage.classList.add('popup_open')
+    })
+    card.querySelector('.element__title').textContent = item.name;
+    let like = card.querySelector('.element__like')
+    like.addEventListener('click',function() {like.classList.toggle('element__like_active')});
+    let clearCard = card.querySelector('.element__clear') 
+    clearCard.addEventListener('click',function() {
+        card.remove();
+        
+    })
+    return card
+  }) 
+  elements.append(...cards)
+}
+
+renderCards(initialCards)
 
 function openPopupCard(){
-    formCard.classList.add('popup_open');
+    popupCard.classList.add('popup_open');
 }
 
 function closePopupCard(){
-    formCard.classList.remove('popup_open')
+    popupCard.classList.remove('popup_open')
 }
 
 function closePopup(){
@@ -76,36 +123,7 @@ function handleFormSubmit (evt) {
     closePopup()
 }
 
-function handleCardSubmit (evt) {
-    evt.preventDefault();
-    let name = titleInput.value;
-    let link = urlInput.value;
-    initialCards.unshift({name, link})
-    renderCards(initialCards)
-    closePopupCard()
-}
 
-function renderCards(arr) {
-    arr.forEach((item) => {
-    const card = template.cloneNode(true)
-    card.querySelector('.element__image').src = item.link
-    card.querySelector('.element__image').addEventListener('click', function() {
-      popupImage.src = item.link
-      formImageSubtitle.textContent = item.name
-      formImage.classList.add('popup_open')
-    })
-    card.querySelector('.element__title').textContent = item.name;
-    let like = card.querySelector('.element__like')
-    like.addEventListener('click',function() {like.classList.toggle('element__like_active')});
-    let clearCard = card.querySelector('.element__clear') 
-    clearCard.addEventListener('click',function() {
-        card.remove();
-    })
-    elements.append(card)
-  }) 
-}
-
-renderCards(initialCards)
 
 form.addEventListener('submit', handleFormSubmit); 
 createProfileBtn.addEventListener('click', openPopup);
